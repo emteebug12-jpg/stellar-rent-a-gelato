979e8a841b25c39e3d81c90a6bdbeddc57d6f2e0
emteebug <emteebug12@gmail.com>
2026-07-11 14:37:50 +0100

    test(contracts): add placeholder integration test stub
    Ignored by default until deposit/register_target/poke have real
    implementations to exercise.

---
+++ contracts/rent_gelato/src/test.rs
@@ -0,0 +1,9 @@
+use soroban_sdk::{testutils::Address as _, Address, Env};
+
+#[test]
+#[ignore = "placeholder integration test"]
+fn placeholder_test() {
+    let env = Env::default();
+    let _user = Address::generate(&env);
+    assert!(true);
+}
